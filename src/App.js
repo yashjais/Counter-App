22d4d7135780b8a3aab178c96f208050cf34d375
Yash Jaiswal <51019768+yashjais@users.noreply.github.com>
2024-08-14 14:16:25 +0530

    Merging

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [isCounterStarted, setIsCounterStarted] = useState(false);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
+  }, []);
+
+  const handleStart = () => {
+    if (!isCounterStarted) {
+      const timerId = setInterval(() => {
+        setCounter((prevState) => prevState + 1);
+      }, 1000);
+      timerRef.current = timerId;
+    } else {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    }
+
+    setIsCounterStarted(!isCounterStarted);
+  };
+
+  const handleReset = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    }
+
+    setCounter(0);
+    setIsCounterStarted(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Counter: {counter}</div>
+      <button onClick={handleStart}>{!isCounterStarted ? 'Start' : 'Pause'}</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
